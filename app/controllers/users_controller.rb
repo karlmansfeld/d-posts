8199a274ec21d118de298ad0f4185bfd6b88d425
karlmansfeld <elenfeldgloria@hotmail.co.jp>
2019-04-13 23:35:19 +0900

    add sign up and user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
+
+ def show
+
+   @user = User.find(params[:id])
+
+ end
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Simple App!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -17,5 +26,5 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
-  
+
 end
