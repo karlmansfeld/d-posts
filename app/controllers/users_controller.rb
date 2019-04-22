3653037f7f36f3ed3f81322450bf0ca464dab470
karlmansfeld <elenfeldgloira@hotmail.co.jp>
2019-04-22 03:02:45 +0000

    20190422am

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 
    @user = User.find(params[:id])
    @microposts = @user.microposts.order(created_at: :desc)
+   @favorites = @user.favposts.order(created_at: :desc)
  end
 
   def new
